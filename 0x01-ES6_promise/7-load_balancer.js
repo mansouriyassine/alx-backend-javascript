695ac155478b204a4e81c81f5a947644a83818d6
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-10 03:16:31 +0000

    7

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// 7-load_balancer.js
+
+/**
+ * Returns the value from the first resolved promise.
+ *
+ * @param {Promise} chinaDownload A promise representing a download from China.
+ * @param {Promise} USDownload A promise representing a download from the US.
+ * @return {Promise} A promise that resolves with the value of the first resolved promise.
+ */
+export default function loadBalancer(chinaDownload, USDownload) {
+    return Promise.race([chinaDownload, USDownload]);
+}
