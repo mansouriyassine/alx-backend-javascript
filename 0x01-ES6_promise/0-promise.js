831181664c5d76efd5c3fbc06d5a2e497d70054d
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-10 02:05:54 +0000

    1

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// 0-promise.js
+
+/**
+ * Returns a Promise that resolves with no value.
+ *
+ * @return {Promise<void>} The promise object.
+ */
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        resolve();
+    });
+}
+
+export default getResponseFromAPI;
