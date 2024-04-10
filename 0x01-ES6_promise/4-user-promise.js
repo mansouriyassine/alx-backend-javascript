5ac4fb7f989841735bfabb59f073d451ef8dff97
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-10 03:07:19 +0000

    4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// 4-user-promise.js
+
+/**
+ * Returns a resolved promise with an object containing the given firstName and lastName.
+ *
+ * @param {string} firstName The first name.
+ * @param {string} lastName The last name.
+ * @return {Promise<Object>} A promise that resolves with the user's name details.
+ */
+function signUpUser(firstName, lastName) {
+    return Promise.resolve({ firstName, lastName });
+}
+
+export default signUpUser;
