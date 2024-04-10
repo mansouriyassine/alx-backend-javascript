a7b2833cfc8b97e945f2e7e9e406f0f3d5400c26
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-10 03:24:06 +0000

    100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+// 100-await.js
+import { uploadPhoto, createUser } from './utils';
+
+/**
+ * Calls uploadPhoto and createUser functions asynchronously, returns an object with
+ * their responses or nulls if any of them fails.
+ *
+ * @return {Promise<Object>} An object with photo and user details or nulls.
+ */
+async function asyncUploadUser() {
+    try {
+        const photo = await uploadPhoto();
+        const user = await createUser();
+        return { photo, user };
+    } catch (error) {
+        return { photo: null, user: null };
+    }
+}
+
+export default asyncUploadUser;
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+// 9-try.js
+
+/**
+ * Executes a math function and manages the outcome, capturing any errors.
+ *
+ * @param {Function} mathFunction - The math function to execute.
+ * @return {Array} An array with the result of the math function
+ * or an error message, and a processed message.
+ */
+export default function guardrail(mathFunction) {
+    const queue = [];
+
+    try {
+        const result = mathFunction();
+        queue.push(result);
+    } catch (error) {
+        queue.push(error.message);
+    }
+
+    queue.push('Guardrail was processed');
+
+    return queue;
+}
