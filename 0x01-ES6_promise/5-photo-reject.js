bcfaae45c8eb931aa461dc01a6996e232dc234e1
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-10 03:10:27 +0000

    5

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// 5-photo-reject.js
+
+/**
+ * Returns a Promise that gets rejected with an Error,
+ * indicating the file cannot be processed.
+ *
+ * @param {string} fileName The name of the file to be uploaded.
+ * @return {Promise<Error>} A promise that rejects with an error message.
+ */
+export default function uploadPhoto(fileName) {
+    return new Promise((resolve, reject) => {
+        reject(new Error(`${fileName} cannot be processed`));
+    });
+}
