2c5129a875836956c110bf49f774cf165f26d08f
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-10 03:05:39 +0000

    3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+// 3-all.js
+import { uploadPhoto, createUser } from './utils';
+
+/**
+ * Handles the profile signup process by waiting for both uploadPhoto and createUser
+ * to resolve and then logging the results.
+ */
+function handleProfileSignup() {
+    Promise.all([uploadPhoto(), createUser()])
+        .then(values => {
+            console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+        })
+        .catch(() => {
+            console.log('Signup system offline');
+        });
+}
+
+export default handleProfileSignup;
