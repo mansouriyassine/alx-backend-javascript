adecdfe8fd2183767a81cb5f8476df4900a177b1
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-10 04:02:26 +0000

    8

--- 0x01-ES6_promise/8-main.js
+++ 0x01-ES6_promise/8-main.js
@@ -1,4 +1,8 @@
 import divideFunction from './8-try.js';
 
-console.log(divideFunction(10, 2));
-console.log(divideFunction(10, 0));
+try {
+    console.log(divideFunction(10, 2));
+    console.log(divideFunction(10, 0));
+} catch (error) {
+    console.error(error.message);
+}
