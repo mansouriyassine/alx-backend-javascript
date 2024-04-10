0bb0039c489672f5bf6c107a9230b8dff540fe29
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-10 03:13:51 +0000

    6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+// 6-final-user.js
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+/**
+ * Calls signUpUser and uploadPhoto, waits for both to settle, and returns
+ * an array with the outcomes of each promise.
+ *
+ * @param {string} firstName
+ * @param {string} lastName
+ * @param {string} fileName
+ * @return {Promise<Array<Object>>}
+ */
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(fileName)
+    ]).then(results => results.map(result => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason
+    })));
+}
