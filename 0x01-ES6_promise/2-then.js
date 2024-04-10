404bab984077aa704f5dc2254a4a76604e2157bb
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-10 03:03:06 +0000

    2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+// 2-then.js
+
+/**
+ * Handles a promise from an API call.
+ *
+ * @param {Promise} promise - The promise to handle.
+ * @return {Promise}
+ */
+function handleResponseFromAPI(promise) {
+    return promise
+        .then(() => {
+            console.log('Got a response from the API');
+            return { status: 200, body: 'success' };
+        })
+        .catch(() => {
+            console.log('Got a response from the API');
+            return new Error();
+        });
+}
+
+export default handleResponseFromAPI;
