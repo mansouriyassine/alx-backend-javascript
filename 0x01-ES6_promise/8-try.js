23d9a6718625dddef4c864a8b146e7722be175c0
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-10 03:18:50 +0000

    8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+// 8-try.js
+
+/**
+ * Divides the numerator by the denominator.
+ * Throws an error if the denominator is zero.
+ *
+ * @param {Number} numerator - The numerator in the division.
+ * @param {Number} denominator - The denominator in the division.
+ * @return {Number} The result of the division.
+ * @throws {Error} When the denominator is 0.
+ */
+export default function divideFunction(numerator, denominator) {
+    if (denominator === 0) {
+        throw new Error('cannot divide by 0');
+    }
+    return numerator / denominator;
+}
