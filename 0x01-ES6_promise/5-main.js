3fbd1dd5ad74566551d75bfec6828e0d7a5456f0
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-10 04:13:40 +0000

    5

--- 0x01-ES6_promise/5-main.js
+++ 0x01-ES6_promise/5-main.js
@@ -1,1 +1,6 @@
 
+import uploadPhoto from './5-photo-reject.js';
+
+uploadPhoto('guillaume.jpg')
+    .catch(error => console.error(error.message))
+    .finally(() => console.log("Operation completed."));
--- 0x01-ES6_promise/5-mains.js
+++
@@ -1,4 +0,0 @@
-import uploadPhoto from './5-photo-reject.js';
-
-uploadPhoto('guillaume.jpg')
-    .catch(error => console.error(error.message));
