64347ad6a38668293572cfb5499995e5ca170e2f
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-11 21:34:00 +0000

    0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-      this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
+}
