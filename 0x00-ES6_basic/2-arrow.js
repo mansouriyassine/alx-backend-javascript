d8b38bc6e80835c4948cc7e1b176e6d8b016982e
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-11 01:59:23 +0000

    0

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -3,7 +3,9 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   this.addNeighborhood = (newNeighborhood) => {
+    console.log('Before adding:', this.sanFranciscoNeighborhoods);
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    console.log('After adding:', this.sanFranciscoNeighborhoods);
     return this.sanFranciscoNeighborhoods;
   };
 }
