b1d5bb20521ab5d4de67b1235acc55fb0e4306a4
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-12 01:10:32 +0000

    5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -8,6 +8,7 @@ class Building {
     return this._sqft;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
