1d3807307b25496e70f7402955855ed212044f21
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-12 00:50:34 +0000

    4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,34 +2,32 @@
 import Currency from './3-currency.js';
 
 class Pricing {
-  constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
-  }
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
+    }
 
-  get amount() {
-    return this._amount;
-  }
+    get amount() {
+        return this._amount;
+    }
+    set amount(newAmount) {
+        this._amount = newAmount;
+    }
 
-  set amount(newAmount) {
-    this._amount = newAmount;
-  }
+    get currency() {
+        return this._currency;
+    }
+    set currency(newCurrency) {
+        this._currency = newCurrency;
+    }
 
-  get currency() {
-    return this._currency;
-  }
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    }
 
-  set currency(newCurrency) {
-    this._currency = newCurrency;
-  }
-
-  displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-  }
-
-  static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
-  }
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
 }
 
 export default Pricing;
