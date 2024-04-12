5664e4e90fb588939401fe756bf1c641adb8d78a
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-12 03:31:23 +0000

    1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -4,22 +4,22 @@
 /**
  * Returns a promise that either resolves with success information or
  * rejects with an error message based on the input boolean.
- * 
+ *
  * @param {boolean} success - Determines whether the promise resolves or rejects.
- * @return {Promise<{status: number, body: string}> | Promise<Error>} - A promise 
+ * @return {Promise<{status: number, body: string}> | Promise<Error>} - A promise
  * that resolves with an object or rejects with an error.
  */
 function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                status: 200,
-                body: 'Success'
-            });
-        } else {
-            reject(new Error('The fake API is not working currently'));
-        }
-    });
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
 
 export default getFullResponseFromAPI;
