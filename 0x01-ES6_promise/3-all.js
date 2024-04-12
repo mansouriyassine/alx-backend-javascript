88d5c10dba23051106cdbe1bc84f6caa464a2edf
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-12 03:37:32 +0000

    3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,19 +6,19 @@ import { uploadPhoto, createUser } from './utils';
  * to resolve and then logging the results.
  */
 function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()])
-        .then(values => {
-            console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-        })
-        .catch(errors => {
-            errors.forEach(error => {
-                if (error instanceof Error) {
-                    console.error(error.message);
-                } else {
-                    console.error('Unknown error occurred');
-                }
-            });
-        });
+  Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    })
+    .catch((errors) => {
+      errors.forEach((error) => {
+        if (error instanceof Error) {
+          console.error(error.message);
+        } else {
+          console.error('Unknown error occurred');
+        }
+      });
+    });
 }
 
 export default handleProfileSignup;
