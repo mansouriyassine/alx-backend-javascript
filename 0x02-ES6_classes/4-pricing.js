91e74984de51868e19ba197a336e0361b732156a
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-12 01:04:01 +0000

    4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-import Currency from './3-currency'; // Remove the file extension and the curly braces
+// eslint-disable-next-line no-unused-vars
+import Currency from './3-currency';
 
 class Pricing {
     constructor(amount, currency) {
