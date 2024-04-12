7f84d5294fc68f9fafe4f670ab0b4b9ba76ccb92
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-12 16:17:14 +0000

    1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,9 @@ export default function taskBlock(trueOrFalse) {
 	let task2 = true;
   
 	if (trueOrFalse) {
-	  task = true;
-	  task2 = false;
+		task = true;
+		task2 = false;
 	}
   
 	return [task, task2];
-  }  
+  } 
