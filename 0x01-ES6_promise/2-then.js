283664ba157d098a2cda0ca72c65ff57ddaddb9e
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-12 03:32:32 +0000

    2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -8,15 +8,15 @@
  * @return {Promise}
  */
 function handleResponseFromAPI(promise) {
-    return promise
-        .then(() => {
-            console.log('Got a response from the API');
-            return { status: 200, body: 'success' };
-        })
-        .catch(() => {
-            console.log('Got a response from the API');
-            return new Error();
-        });
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
 
 export default handleResponseFromAPI;
