8e2a14d357ac7aafc417eb934c8ce90dd673e43c
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-12 03:46:29 +0000

    100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
-// 100-await.js
-import { uploadPhoto, createUser } from './utils.js';
-
-/**
- * Calls uploadPhoto and createUser functions asynchronously, returns an object with
- * their responses or nulls if any of them fails.
- *
- * @return {Promise<Object>} An object with photo and user details or nulls.
- */
+import { uploadPhoto, createUser } from './utils';
 async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
