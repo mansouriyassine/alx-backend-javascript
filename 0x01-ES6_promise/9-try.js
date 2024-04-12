613c0ec1767cf566225005495a7cab9169280f54
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-12 03:43:22 +0000

    9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,4 @@
 #!/usr/bin/node
-// 9-try.js
-
-/**
- * Executes a math function and manages the outcome, capturing any errors.
- *
- * @param {Function} mathFunction - The math function to execute.
- * @return {Array} An array with the result of the math function
- * or an error message, and a processed message.
- */
 export default function guardrail(mathFunction) {
     const queue = [];
 
