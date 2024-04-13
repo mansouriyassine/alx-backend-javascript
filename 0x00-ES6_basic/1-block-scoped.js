cc443935cc5a9fb1bf27193e7e2f587920a4e4e9
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-13 22:15:58 +0000

    1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,20 +1,6 @@
-/* eslint-disable no-var, block-scoped-var, vars-on-top, no-redeclare */
 export default function taskBlock(trueOrFalse) {
-	  var task = false;
-	  var task2 = true;
-
-<<<<<<< HEAD
-	  if (trueOrFalse) {
-		      // Conditional block is empty to maintain initial values
-		  //   }
-		  //
-		  //     return [task, task2];
-		  //     }
-=======
-  if (trueOrFalse) {
-    // Conditional block is empty to maintain initial values
-  }
+  const task = false;
+  const task2 = true;
 
   return [task, task2];
 }
->>>>>>> 9dbdb2154ead6b586d35aa25614d9ad179319a16
