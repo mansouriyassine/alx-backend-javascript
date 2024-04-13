b6c4a74e3dd4fec5149f2ebae848c4236c6f6ead
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-13 21:42:11 +0000

    5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,21 +1,14 @@
 class Building {
     constructor(sqft) {
-        if (new.target === Building) {
-            throw new TypeError("Cannot construct Building instances directly");
-        }
-        if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-            throw new Error("Class extending Building must override evacuationWarningMessage");
-        }
-        this._sqft = sqft;
+      this._sqft = sqft;
     }
-
+  
     get sqft() {
-        return this._sqft;
+      return this._sqft;
     }
-
-    // eslint-disable-next-line class-methods-use-this
+  
     evacuationWarningMessage() {
-        throw new Error('Method "evacuationWarningMessage" must be implemented');
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 }
 
