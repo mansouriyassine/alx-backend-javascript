0eb66709c4635d9a4e252d7da00ef0ad649646c8
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-13 21:03:18 +0000

    6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map(result => ({
+    .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : String(result.reason),
     })));
