0305d18475c15acb68f0bc2b0c8997ef7ea7ca3c
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-13 19:20:44 +0000

    all

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+export default function createReportObject(employeesList) {
+	const allEmployees = { ...employeesList };
+
+	function getNumberOfDepartments() {
+		return Object.keys(allEmployees).length;
+	}
+
+	return {
+		allEmployees,
+		getNumberOfDepartments,
+	};
+}
