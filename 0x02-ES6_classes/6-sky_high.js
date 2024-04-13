d1a12d68f46899da833a8f46d72c6c4203ff42ec
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-13 22:44:33 +0000

    6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,19 +1,25 @@
-#!/usr/bin/node
 import Building from './5-building';
 
 class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
-    super(sqft);
-    this._floors = floors;
-  }
+    constructor(sqft, floors) {
+        super(sqft);
+        this._sqft = sqft;
+        this._floors = floors;
+    }
 
-  get floors() {
-    return this._floors;
-  }
+    // Getter for the sqft attribute
+    get sqft() {
+        return this._sqft;
+    }
 
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
-  }
+    // Getter for the floors attribute
+    get floors() {
+        return this._floors;
+    }
+
+    evacuationWarningMessage() {
+        return `Evacuate slowly the ${this._floors} floors`;
+    }
 }
 
 export default SkyHighBuilding;
