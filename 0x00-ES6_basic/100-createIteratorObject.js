76733227a24d0d805212e1bd73e0766f8520177e
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-13 19:53:37 +0000

    100 101

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 export default function createIteratorObject(report) {
-	const departments = Object.values(report.allEmployees);
-  
-	function* iterateEmployees() {
-		for (const department of departments) {
-			for (const employee of department) {
-				yield employee;
-			}
-		}
-	}
-  
-	return {
-		[Symbol.iterator]: iterateEmployees
-	};
-}  
+  const departments = Object.values(report.allEmployees);
+
+  function* iterateEmployees() {
+    for (const department of departments) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }
+
+  return {
+    [Symbol.iterator]: iterateEmployees,
+  };
+}
