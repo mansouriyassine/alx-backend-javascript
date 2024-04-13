12aa992964aed6cd9a15ec20fc1b6161b2e4400d
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-13 20:47:09 +0000

    3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,14 +2,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photo, user] = results;
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
-    .catch((error) => {
-      console.error('Signup system offline');
-    });
+    Promise.all([uploadPhoto(), createUser()])
+        .then((results) => {
+            const [photo, user] = results;
+            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+        })
+        .catch((error) => {
+            console.error('Signup system offline:', error.message);
+        });
 }
 
 export default handleProfileSignup;
