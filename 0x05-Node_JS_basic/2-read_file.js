a22954e0c991b722c1595ceaf7da7abad9cd3aef
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-03 14:52:31 +0100

    2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,35 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+
+    if (lines.length === 0) {
+      throw new Error('Cannot load the database');
+    }
+
+    const students = lines.slice(1); // Skip the header line
+    const totalStudents = students.length;
+
+    console.log(`Number of students: ${totalStudents}`);
+
+    const fields = {};
+
+    students.forEach((student) => {
+      const [firstname, lastname, age, field] = student.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    });
+
+    for (const [field, students] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
