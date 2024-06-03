7f1c91a4983418c9c7bbd0e224d1e442a23b4071
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-03 17:17:52 +0100

    8

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,27 +1,25 @@
 import fs from 'fs';
 
-const readDatabase = (filePath) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-      } else {
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
-        const students = lines.slice(1); // Skip header line
-        const fields = {};
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    } else {
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1); // Skip header line
+      const fields = {};
 
-        students.forEach((student) => {
-          const [firstname, lastname, age, field] = student.split(',');
-          if (!fields[field]) {
-            fields[field] = [];
-          }
-          fields[field].push(firstname);
-        });
+      students.forEach((student) => {
+        const [firstname, lastname, age, field] = student.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      });
 
-        resolve(fields);
-      }
-    });
+      resolve(fields);
+    }
   });
-};
+});
 
 export { readDatabase };
