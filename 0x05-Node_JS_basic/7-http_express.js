44ff77451e36301d39e4e84d037bc981c525bfa3
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-03 15:24:38 +0100

    7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,1 +1,34 @@
-#!/usr/bin/node
+const express = require('express');
+const fs = require('fs');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+
+  const databasePath = req.query.database || 'database.csv';
+
+  countStudents(databasePath)
+    .then(() => {
+      res.end();
+    })
+    .catch((error) => {
+      res.write(error.message);
+      res.end();
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = app;
