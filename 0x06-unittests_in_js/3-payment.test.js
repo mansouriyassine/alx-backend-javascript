bc324b8632f753e719dfd6cb6fa608504aba7227
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-05 15:36:29 +0100

    3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,28 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+    it('should call Utils.calculateNumber with SUM type and correct arguments', () => {
+        const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+        sendPaymentRequestToApi(100, 20);
+
+        expect(calculateNumberSpy.calledOnce).to.be.true;
+        expect(calculateNumberSpy.calledWithExactly('SUM', 100, 20)).to.be.true;
+
+        calculateNumberSpy.restore();
+    });
+
+    it('should log the correct total amount', () => {
+        const consoleSpy = sinon.spy(console, 'log');
+
+        sendPaymentRequestToApi(100, 20);
+
+        expect(consoleSpy.calledOnce).to.be.true;
+        expect(consoleSpy.calledWithExactly('The total is: 120')).to.be.true;
+
+        consoleSpy.restore();
+    });
+});
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,1 +1,22 @@
-#!/usr/bin/node
+const Utils = {
+    calculateNumber: function(type, a, b) {
+        const roundedA = Math.round(a);
+        const roundedB = Math.round(b);
+
+        switch (type) {
+            case 'SUM':
+                return roundedA + roundedB;
+            case 'SUBTRACT':
+                return roundedA - roundedB;
+            case 'DIVIDE':
+                if (roundedB === 0) {
+                    return 'Error';
+                }
+                return roundedA / roundedB;
+            default:
+                throw new Error('Invalid operation type. Supported types are SUM, SUBTRACT, and DIVIDE.');
+        }
+    }
+};
+
+module.exports = Utils;
