7e685881bb95e2e98df1481b548ac16afd93f6cc
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-05 15:50:29 +0100

    6

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+function getPaymentTokenFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({ data: 'Successful response from the API' });
+        } else {
+            reject(new Error('API call failed'));
+        }
+    });
+}
+
+module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+const assert = require('assert');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+    it('should resolve with success message when success is true', (done) => {
+        getPaymentTokenFromAPI(true)
+            .then((response) => {
+                assert.deepStrictEqual(response, { data: 'Successful response from the API' });
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
+
+    it('should reject with an error when success is false', (done) => {
+        getPaymentTokenFromAPI(false)
+            .then(() => {
+                done(new Error('Expected promise to be rejected'));
+            })
+            .catch((err) => {
+                assert.strictEqual(err.message, 'API call failed');
+                done();
+            });
+    });
+});
