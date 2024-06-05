4c04fceb6493c9a09b0e8657b0bfdf79616caa2a
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-05 15:38:49 +0100

    3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,25 +4,20 @@ const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
+    afterEach(() => {
+        sinon.restore();
+    });
+
     it('should call Utils.calculateNumber with SUM type and correct arguments', () => {
         const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+        const consoleSpy = sinon.spy(console, 'log');
 
         sendPaymentRequestToApi(100, 20);
 
         expect(calculateNumberSpy.calledOnce).to.be.true;
         expect(calculateNumberSpy.calledWithExactly('SUM', 100, 20)).to.be.true;
 
-        calculateNumberSpy.restore();
-    });
-
-    it('should log the correct total amount', () => {
-        const consoleSpy = sinon.spy(console, 'log');
-
-        sendPaymentRequestToApi(100, 20);
-
         expect(consoleSpy.calledOnce).to.be.true;
         expect(consoleSpy.calledWithExactly('The total is: 120')).to.be.true;
-
-        consoleSpy.restore();
     });
 });
