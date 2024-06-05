dbf5d69c26e8f1e3d6aadf2237e6200986b3338a
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-05 15:48:29 +0100

    5

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,27 @@
-#!/usr/bin/node
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+    let consoleSpy;
+
+    beforeEach(() => {
+        consoleSpy = sinon.spy(console, 'log');
+    });
+
+    afterEach(() => {
+        consoleSpy.restore();
+    });
+
+    it('should log the correct message for totalAmount = 100 and totalShipping = 20', () => {
+        sendPaymentRequestToApi(100, 20);
+        expect(consoleSpy.calledOnce).to.be.true;
+        expect(consoleSpy.calledWithExactly('The total is: 120')).to.be.true;
+    });
+
+    it('should log the correct message for totalAmount = 10 and totalShipping = 10', () => {
+        sendPaymentRequestToApi(10, 10);
+        expect(consoleSpy.calledOnce).to.be.true;
+        expect(consoleSpy.calledWithExactly('The total is: 20')).to.be.true;
+    });
+});
