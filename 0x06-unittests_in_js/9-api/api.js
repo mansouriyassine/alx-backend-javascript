fbe04d2c0dee9668b36d9faccbb52cc6a207992e
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-05 16:00:40 +0100

    9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+const PORT = 7865;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the payment system');
+});
+
+// New endpoint for /cart/:id
+app.get('/cart/:id', (req, res) => {
+  const { id } = req.params;
+
+  // Validate that id is a number
+  if (!/^\d+$/.test(id)) {
+    return res.status(404).send('Cart ID must be a number');
+  }
+
+  res.status(200).send(`Payment methods for cart ${id}`);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
+
+module.exports = server;
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,1 +1,79 @@
-#!/usr/bin/node
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('./api');
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe('Server', () => {
+  after(() => {
+    server.close();
+  });
+
+  describe('Index page', () => {
+    it('Correct status code?', (done) => {
+      chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+
+    it('Correct result?', (done) => {
+      chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          expect(res.text).to.equal('Welcome to the payment system');
+          done();
+        });
+    });
+
+    it('Other?', (done) => {
+      chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          // Add additional tests here if needed
+          done();
+        });
+    });
+  });
+
+  describe('Cart page', () => {
+    it('Correct status code when :id is a number?', (done) => {
+      chai.request(server)
+        .get('/cart/12')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+
+    it('Correct result when :id is a number?', (done) => {
+      chai.request(server)
+        .get('/cart/12')
+        .end((err, res) => {
+          expect(res.text).to.equal('Payment methods for cart 12');
+          done();
+        });
+    });
+
+    it('Correct status code when :id is NOT a number (=> 404)?', (done) => {
+      chai.request(server)
+        .get('/cart/hello')
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          done();
+        });
+    });
+
+    it('Correct result when :id is NOT a number (=> 404)?', (done) => {
+      chai.request(server)
+        .get('/cart/hello')
+        .end((err, res) => {
+          expect(res.text).to.equal('Cart ID must be a number');
+          done();
+        });
+    });
+  });
+});
