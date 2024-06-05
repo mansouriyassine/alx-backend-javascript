2e296612c856d2879615e9225e97c261091e99cc
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-05 15:56:03 +0100

    8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,34 @@
-#!/usr/bin/node
+const { expect } = require('chai');
+const request = require('request');
+const server = require('./api');
+
+describe('Index page', () => {
+  let baseUrl = 'http://localhost:7865';
+
+  before(() => {
+    server;
+  });
+
+  it('Correct status code?', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result?', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Other?', (done) => {
+    // Add more tests if needed
+    done();
+  });
+
+  after(() => {
+    server.close();
+  });
+});
