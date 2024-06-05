865451751bbd6f8d280fdcc9f0084394b6eca0ad
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-05 15:30:58 +0100

    1

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,1 +1,33 @@
-#!/usr/bin/node
+const chai = require('chai');
+const expect = chai.expect;
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', () => {
+    describe('SUM operation', () => {
+        it('should return the correct sum of rounded numbers', () => {
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+        });
+    });
+
+    describe('SUBTRACT operation', () => {
+        it('should return the correct subtraction of rounded numbers', () => {
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+        });
+    });
+
+    describe('DIVIDE operation', () => {
+        it('should return the correct division of rounded numbers', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+        });
+
+        it('should return "Error" if the rounded value of b is 0', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+        });
+    });
+
+    describe('Error cases', () => {
+        it('should throw an error for an invalid operation type', () => {
+            expect(() => calculateNumber('INVALID', 1.4, 4.5)).to.throw(Error);
+        });
+    });
+});
