bffb1a8c4c892192d111b5c9336538443c860002
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-05 14:57:17 +0100

    5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,13 +1,14 @@
 const http = require('http');
+const url = require('url');
 const fs = require('fs');
 const path = require('path');
 
 // Function to read and parse the CSV file
-const readDatabase = (filePath) => {
+const countStudents = (filePath) => {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf-8', (err, data) => {
       if (err) {
-        reject(err);
+        reject(new Error('Cannot load the database'));
         return;
       }
 
@@ -22,38 +23,37 @@ const readDatabase = (filePath) => {
         students.push({ id, firstName, lastName, field });
       });
 
-      resolve(students);
+      const csStudents = students.filter(student => student.field === 'CS');
+      const sweStudents = students.filter(student => student.field === 'SWE');
+
+      const responseText = `This is the list of our students
+Number of students: ${students.length}
+Number of students in CS: ${csStudents.length}. List: ${csStudents.map(student => student.firstName).join(', ')}
+Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map(student => student.firstName).join(', ')}`;
+
+      resolve(responseText);
     });
   });
 };
 
 // Create the server
 const app = http.createServer((req, res) => {
-  const { url } = req;
+  const parsedUrl = url.parse(req.url, true);
 
-  if (url === '/') {
+  if (parsedUrl.pathname === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  } else if (url === '/students') {
-    const databasePath = path.resolve(process.argv[2]);
+  } else if (parsedUrl.pathname === '/students') {
+    const databasePath = process.argv[2];
 
-    readDatabase(databasePath)
-      .then((students) => {
-        const csStudents = students.filter(student => student.field === 'CS');
-        const sweStudents = students.filter(student => student.field === 'SWE');
-
-        const responseText = `This is the list of our students
-Number of students: ${students.length}
-Number of students in CS: ${csStudents.length}. List: ${csStudents.map(student => student.firstName).join(', ')}
-Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map(student => student.firstName).join(', ')}`;
-
+    countStudents(databasePath)
+      .then((data) => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(responseText);
+        res.end(data);
       })
-      .catch((err) => {
+      .catch((error) => {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-        console.error('Error reading database file:', err);
+        res.end(error.message);
       });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -62,8 +62,9 @@ Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map(studen
 });
 
 // Listen on port 1245
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 module.exports = app;
